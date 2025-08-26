4e9f5ae579075d5d2f9f99768c452954c1ce07d2
Ishita Srivastav <104553220+Ishu-sri-001@users.noreply.github.com>
2025-08-26 19:11:10 +0530

    changes

--- src/components/UI/IntroBox.jsx
+++ src/components/UI/IntroBox.jsx
@@ -13,22 +13,22 @@ export default function IntroBox({
   setShowIntroBox,
   setShowSidebar,
 }) {
-  //   useEffect(() => {
-  //   gsap.to("#greenPath", {
-  //     duration: 3,
-  //     repeat: -1,
-  //     yoyo: true,
-  //     ease: "power1.inOut",
-  //     motionPath: {
-  //       path: "#circlePath",     
-  //       align: "#circlePath",    
-  //       alignOrigin: [0.5, 0.5],
-  //       autoRotate: true,        
-  //       start: 0,
-  //       end: 0.5  
-  //     }
-  //   })
-  // }, [])
+    useEffect(() => {
+    gsap.to("#greenPath", {
+      duration: 1.5,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+      motionPath: {
+        path: "#circlePath",     
+        align: "#circlePath",    
+        alignOrigin: [-0.4, 0.7],
+        autoRotate: true,        
+        start: -0.2,
+        end: 0.08  
+      }
+    })
+  }, [])
   const handleEnter = () => {
     const tl = gsap.timeline();
     tl.to(".enter-container", {
@@ -123,7 +123,7 @@ export default function IntroBox({
                     transform="matrix(1.9746062755584717,-0.317694753408432,0.317694753408432,1.9746062755584717,0,0)"
                   >
                     <path
-                    className="rotate-[-90deg]"
+                   
                       id="greenPath"
                       strokeLinecap="butt"
                       strokeLinejoin="round"
